f1cde9d6ee128155cf720d10a47241547670630d
rvelasquezc <rvelasquezumg@gmail.com>
2022-03-24 21:41:25 -0600

    Html table added

--- JS-001/index-v2.js
+++ JS-001/index-v2.js
@@ -1,3 +1,15 @@
+const form = document.getElementsByTagName("form")[0];
+/**@type {HTMLInputElement} */
+const inputCodigo = document.getElementById("codigo");
+/**@type {HTMLInputElement} */
+const inputNombre = document.getElementById("nombre");
+/**@type {HTMLInputElement} */
+const inputCantidad = document.getElementById("cantidad");
+/**@type {HTMLInputElement} */
+const inputPrecio = document.getElementById("precio");
+/**@type {HTMLInputElement} */
+const inputCategoria = document.getElementById("categoria");
+const tbody = document.getElementsByTagName("tbody")[0];
 
 const preloadedState = {
     producto:{},
@@ -34,10 +46,37 @@ store.subscribe(() =>{
     {
         latestState = currentState;
         console.log("estado: ", store.getState());
+        renderTable(currentState.productos);
     }
-
-    
 })
+
+function renderTable(productos)
+{
+   const filas = productos.map((item) => {
+
+    const  tr = document.createElement("tr");
+    tr.innerHTML = `
+    <td>${item.codigo}</td>
+    <td>${item.nombre}</td>
+    <td>${item.cantidad}</td>
+    <td>${item.precio}</td>
+    <td>${item.categoria}</td>
+    <td>${item.total}</td>
+    <td>
+        <div class="btn-group">
+            <a title ="Editar" href="#" onclick="onEdit(event)" class="btn btn-sm btn-outline-secondary"><i class="bi bi-pencil-square"></i></a> 
+            <a title ="Eliminar" href="#" onclick="onDelete(event)" class="btn btn-sm btn-outline-danger"><i class="bi bi-trash"></i></a>
+        </div>
+    </td>
+    `;
+    return tr;
+    });
+
+    filas.forEach(tr => {
+        tbody.appendChild(tr);
+    });
+   
+}
 
 store.dispatch({
     type: "producto-agregado",
