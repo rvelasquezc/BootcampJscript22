29f5da51032a81ca4776147daeb646c26d64801b
rvelasquezc <rvelasquezumg@gmail.com>
2022-03-25 00:19:44 -0600

    Manage events and buil form

--- JS-001/index-v2.js
+++ JS-001/index-v2.js
@@ -8,26 +8,66 @@ const inputCantidad = document.getElementById("cantidad");
 /**@type {HTMLInputElement} */
 const inputPrecio = document.getElementById("precio");
 /**@type {HTMLInputElement} */
+
 const inputCategoria = document.getElementById("categoria");
 const tbody = document.getElementsByTagName("tbody")[0];
+//Asignar valor por Id a Html
+const cantidadTotalElement = document.getElementById("cantidad_total"); 
+const precioTotalElement = document.getElementById("precio_total"); 
+const granTotalElement = document.getElementById("gran_total");
 
 const preloadedState = {
     producto:{},
     productos: []
 };
 
+let indice = 0;
 const reducer = (state, action) => {
     if (action.type == "producto-agregado")
     {
-
         /**state.productos.push(action.payload);*/
+        indice ++;
+        const producto = action.payload;
+        const codigo = indice;
+        const total = producto.cantidad * producto.precio;
         return { 
             ...state, 
             productos: [
                 ...state.productos, 
-                action.payload
+                {
+                ...producto,
+                codigo,
+                total
+                }              
             ]
         };
+    }
+
+    if (action.type == "producto-modificado")
+    {
+        const producto = action.payload;
+        const productos = state.productos.slice(); 
+        const codigo = producto.codigo;
+        const total = producto.cantidad * producto.precio;
+        const old = productos.find((item) => item.codigo == codigo);
+        const index = productos.indexOf(old);
+        productos[index] = {...producto, total };
+
+        return{
+
+            ...state,
+            productos
+        };
+    }
+
+    if (action.type == "producto-eliminado")
+    {
+        const codigo = action.payload.codigo;
+        const productos = state.productos.filter((item) => item.codigo != codigo);
+        return {
+            ...state,
+            productos
+        }
     }
 
     return state;
@@ -39,7 +79,7 @@ const store = Redux.createStore(reducer, preloadedState);
 
  let latestState;
 
-store.subscribe(() =>{
+const unsuscribe =  store.subscribe(() =>{
     //console.log("suscribe ejecutando");
     let currentState = store.getState();
     if(currentState != latestState)
@@ -64,42 +104,153 @@ function renderTable(productos)
     <td>${item.total}</td>
     <td>
         <div class="btn-group">
-            <a title ="Editar" href="#" onclick="onEdit(event)" class="btn btn-sm btn-outline-secondary"><i class="bi bi-pencil-square"></i></a> 
-            <a title ="Eliminar" href="#" onclick="onDelete(event)" class="btn btn-sm btn-outline-danger"><i class="bi bi-trash"></i></a>
+            <a title ="Editar" href="#" class="btn btn-sm btn-outline-secondary">
+            <i class="bi bi-pencil-square"></i>
+            </a> 
+            <a title ="Eliminar" href="#" class="btn btn-sm btn-outline-danger">
+            <i class="bi bi-trash"></i>
+            </a>
         </div>
     </td>
     `;
+
+    const [editar, eliminar] = tr.getElementsByTagName("a");
+
+    eliminar.addEventListener("click", (event) => {
+        event.preventDefault();
+        store.dispatch({
+            type: "producto-eliminado",
+            payload: {
+                codigo: item.codigo
+            }
+        });
+    });
+
+    editar.addEventListener("click", (event) => {
+        event.preventDefault();
+        store.dispatch({
+            type: "producto-seleccionado",
+            payload: {
+                codigo: item.codigo
+            }
+        });
+    });
+
     return tr;
     });
 
+    tbody.innerHTML = "";
     filas.forEach(tr => {
         tbody.appendChild(tr);
     });
+
+    cantidadTotalElement.innerText = sum(productos, x => x.cantidad);
+    precioTotalElement.innerText = sum(productos, x => x.precio);
+    granTotalElement.innerText = sum(productos, x => x.total);
+
+    function sum(elementos, selector){
+        return elementos
+        .map(selector)
+        .reduce((a,b) => a + b, 0);
+    }
    
 }
 
-store.dispatch({
+/**
+ * 
+ * @param {Event} event 
+ */
+ function onSubmit(event)
+ {
+     event.preventDefault();
+ 
+     const data = new FormData(form);
+     const values = Array.from(data.entries());
+ 
+     const [frmCodigo, frmNombre, frmCantidad, frmPrecio, frmCategoria] = values;
+ 
+     const codigo = parseInt(frmCodigo[1]);
+     const nombre = frmNombre[1];
+     const cantidad = parseFloat(frmCantidad[1]);
+     const precio = parseFloat(frmPrecio[1]);
+     const categoria = parseInt(frmCategoria[1]);
+
+     if (codigo)
+     {
+         store.dispatch({
+             type: "producto-modificado",
+             payload: {
+                 codigo,
+                 nombre,
+                 cantidad,
+                 precio,
+                 categoria
+             }
+         });
+     }
+     else {
+         store.dispatch({
+             type: "producto-agregado",
+             payload: {
+                 nombre,
+                 cantidad,
+                 precio,
+                 categoria
+             }
+         });
+     }
+
+     form.reset();
+
+ }
+
+store.dispatch({ 
     type: "producto-agregado",
     payload: {
-        id: 1,
-        nombre: "prueba"
+        nombre: "prueba a",
+        cantidad: 3,
+        precio: 10,
+        categoria: 2
     }
-})
+});
 
-store.dispatch({
-    type: "producto-agregado",
-    payload: {
-        id: 2,
-        nombre: "prueba2"
-    }
-})
-
-store.dispatch({
+store.dispatch({ 
     type: "producto-modificado",
     payload: {
-        id: 2,
-        nombre: "prueba2 v2"
+        codigo: 1,
+        nombre: "prueba a v2",
+        cantidad: 4,
+        precio: 11,
+        categoria: 1
     }
-})
+});
+
+store.dispatch({ 
+    type: "producto-agregado",
+    payload: {
+        nombre: "prueba b",
+        cantidad: 6,
+        precio: 8,
+        categoria: 3
+    }
+});
+
+store.dispatch({ 
+    type: "producto-agregado",
+    payload: {
+        nombre: "prueba c",
+        cantidad: 2,
+        precio: 4,
+        categoria: 4
+    }
+});
+
+store.dispatch({
+    type: "producto-eliminado",
+    payload: {
+        codigo: 3
+    }
+});
+
 
 //console.log(store);
